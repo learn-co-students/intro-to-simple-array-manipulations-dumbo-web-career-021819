9669380d99de34372729da2a5304d4e1648119bb
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-26 03:33:06 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat array2
+end
+
+def using_insert(arr, ele)
+  arr.insert(4, ele)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, str)
+  arr.delete(str)
+end
+
+def using_delete_at(arr, n)
+  arr.delete(arr[n])
+end
